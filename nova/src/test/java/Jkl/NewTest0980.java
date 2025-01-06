27ad9c8fca6f1e027ab9da0c16fbf4140cc2234c
Frazina <Frazina@LAPTOP-T5HRFLI1>
2025-01-06 14:35:53 +0530

    bhi

---
+++ nova/src/test/java/Jkl/NewTest0980.java
@@ -0,0 +1,9 @@
+package Jkl;
+
+import org.testng.annotations.Test;
+
+public class NewTest0980 {
+  @Test
+  public void f() {
+  }
+}
---
+++ nova/src/test/java/Ysd/NewTest01234.java
@@ -0,0 +1,92 @@
+package Ysd;
+
+
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import java.io.File;
+import java.time.Duration;
+
+import org.apache.commons.io.FileUtils;
+
+public class NewTest01234 {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+  private JavascriptExecutor js;
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+	  WebDriverManager.chromedriver().setup();
+    driver = new ChromeDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+    js = (JavascriptExecutor) driver;
+  }
+
+  @Test
+  public void testFacebook() throws Exception {
+    driver.get(baseUrl + "chrome://newtab/");
+    driver.get("https://www.facebook.com/");
+    driver.findElement(By.id("email")).click();
+    driver.findElement(By.id("email")).clear();
+    driver.findElement(By.id("email")).sendKeys("aghiop0987654321");
+    driver.findElement(By.name("pass")).click();
+    driver.findElement(By.id("pass")).clear();
+    driver.findElement(By.id("pass")).sendKeys("1234567890");
+    driver.findElement(By.id("u_0_5_LC")).click();
+  }
+
+  @AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
+
+
