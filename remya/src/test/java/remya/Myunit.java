303dffc50ba64356107982fb24a5f1f5de4cc5c1
Frazina <Frazina@LAPTOP-T5HRFLI1>
2025-01-06 20:16:36 +0530

    mjk

---
+++ remya/src/test/java/remya/Myunit.java
@@ -0,0 +1,14 @@
+package remya;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Myunit {
+
+	@Test
+	public void test() {
+	System.out.println("welcome to testing");
+	}
+
+}
