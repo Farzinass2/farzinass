5dd5236e507bebf547814707201bb04ea5b97986
Frazina <Frazina@LAPTOP-T5HRFLI1>
2025-01-06 13:57:35 +0530

    mlp

---
+++ tintu/src/test/java/tintu/Sdr.java
@@ -0,0 +1,14 @@
+package tintu;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Sdr {
+
+	@Test
+	public void test() {
+		System.out.println("Welcome to testing");
+	}
+
+}
---
+++ tintu/src/test/java/tintu/Wrety.java
@@ -0,0 +1,10 @@
+package tintu;
+
+public class Wrety {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to world of testing");
+
+	}
+
+}
