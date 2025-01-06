dde7d5a8ae1dbb0cc8a3c57789986af98f397166
Frazina <Frazina@LAPTOP-T5HRFLI1>
2025-01-06 14:38:37 +0530

    nkl

---
+++ Sam09/src/main/java/forl09/happyllo.java
@@ -0,0 +1,10 @@
+package forl09;
+
+public class happyllo {
+
+	public static void main(String[] args) {
+		System.out.println("Helloworld");
+
+	}
+
+}
---
+++ Sam09/src/main/java/forl09/hrep89.java
@@ -0,0 +1,10 @@
+package forl09;
+
+public class hrep89 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
