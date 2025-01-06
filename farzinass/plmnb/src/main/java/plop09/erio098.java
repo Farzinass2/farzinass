a2aa4040a4a77a66805519d5e052479d7d91e4ad
Frazina <Frazina@LAPTOP-T5HRFLI1>
2025-01-06 19:45:28 +0530

    ml

---
+++ farzinass/plmnb/src/main/java/plop09/erio098.java
@@ -0,0 +1,12 @@
+package plop09;
+
+public class erio098 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello welcome to java");
+
+	}
+
+}
---
+++ farzinass/plmnb/src/main/java/plop09/package-info.java
@@ -0,0 +1,1 @@
+package plop09;
