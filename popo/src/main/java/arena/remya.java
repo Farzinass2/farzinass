91b2e0f1939d9e417806eade5cbbefb48d6a2508
Frazina <Frazina@LAPTOP-T5HRFLI1>
2025-01-06 20:18:43 +0530

    nfg

---
+++ popo/src/main/java/arena/remya.java
@@ -0,0 +1,15 @@
+package arena;
+
+public class remya {
+
+	public static void display()
+	{
+		System.out.println("Hello");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+remya.display();
+	}
+
+}
